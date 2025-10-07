c64ca70a574e7b8bf42b065e7d72674b83d0e131
Igor Budnitski <igor.budnitski@gmail.com>
2025-10-07 19:13:43 +0300

    CORS fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import { router } from './routes.js';
 const port = Number(process.env.PORT) || 3000;
 
 const server = http.createServer((req, res) => {
-    router(req, res).catch((err) => {
-        console.error(err);
-        res.writeHead(500);
-        res.end('Internal Server Error');
-    });
-});
 
-server.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
-});
-/*
-const server = http.createServer((req, res) => {
     // ✅ Разрешаем запросы от любых источников
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -34,4 +23,8 @@ const server = http.createServer((req, res) => {
         res.writeHead(500);
         res.end('Internal Server Error');
     });
-});*/
+});
+
+server.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});

c823ebf9bb9f2fe495276a7678c0980d3308c750
Igor Budnitski <igor.budnitski@gmail.com>
2025-10-09 22:12:13 +0300

    timer app HW 1st part

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,38 @@
-import { parse } from 'url';
-import { getAllTimes, saveCurrentTime, deleteTimeById } from'./repositories/timer.repository.js';
+import {parse} from 'url';
+import {getAllTimes, saveCurrentTime, deleteTimeById, updateTimeById} from './repositories/timer.repository.js';
 
 export async function router(req, res) {
     // Получаем путь (с помощью утилиты parse) и метод из запроса
     const url = parse(req.url || '', true);
     const method = req.method;
 
+    console.log(`Url path: ${url.path}`);
+    console.log(url);
+    // getting id value from URL
+    const idParam = url.pathname.split('/')[2];
+    // getting saved_at time
+    const saved_At = url.query.saved_at;
 
+    if (url.query.saved_at && method === 'PUT') {
+        const result = await updateTimeById(idParam, saved_At);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result));
+        return;
+    }
 
     // Обработка запроса: GET /timer
     if (url.pathname === '/timer' && method === 'GET') {
         const times = await getAllTimes();
 
         // Говорим клиенту: "Всё ок, вот JSON"
-        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(times)); // Отправляем массив записей в формате JSON
         return;
     }
 
     if (url.pathname === '/timer/save' && method === 'POST') {
         const time = await saveCurrentTime();
-        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.writeHead(201, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(time));
         return;
     }
@@ -28,11 +40,11 @@ export async function router(req, res) {
     if (url.pathname?.startsWith('/timer/') && method === 'DELETE') {
         const id = url.pathname.split('/')[2];
         await deleteTimeById(id);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: `Deleted time with ID ${id}` }));
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: `Deleted time with ID ${id}`}));
         return;
     }
 
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('Not found');
 }
